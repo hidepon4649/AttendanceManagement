f4b2e34a09fc91fe7e0a9709ec0e1bc6b5812a3a
Hidenori Mishima <mishima.hidenori@gmail.com>
2024-10-15 11:22:46 +0900

    ログイン画面の見栄えを微調整

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -18,9 +18,30 @@ const LoginForm = () => {
   return (
     <div className="mt-3">
       <h2>ログイン</h2>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="メールアドレス" />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="パスワード" />
-      <button onClick={handleLogin}>ログイン</button>
+      <div className="form-group">
+        <label htmlFor="email">メールアドレス:</label>
+        <input 
+          type="email" 
+          className="form-control"
+          id="email"
+          autoComplete="email"
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          placeholder="メールアドレス" 
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">パスワード:</label>
+          <input 
+            type="password" 
+            className="form-control"
+            id="password"
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+            placeholder="パスワード" 
+          />
+        </div>
+        <button className="btn btn-primary" onClick={handleLogin}>ログイン</button>
     </div>
   );
 };
