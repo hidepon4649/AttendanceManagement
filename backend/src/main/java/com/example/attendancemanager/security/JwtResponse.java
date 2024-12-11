990f0978fce1bb785d30bb6f79b4073184f4efc6
Hidenori Mishima <mishima.hidenori@gmail.com>
2024-12-12 06:26:03 +0900

    コメントアウトの残骸を掃除

--- backend/src/main/java/com/example/attendancemanager/security/JwtResponse.java
+++ backend/src/main/java/com/example/attendancemanager/security/JwtResponse.java
@@ -39,20 +39,3 @@ public class JwtResponse {
     }
 
 }
-// package com.example.attendancemanager.model;
-
-// public class JwtResponse {
-// private String token;
-
-// public JwtResponse(String token) {
-// this.token = token;
-// }
-
-// public String getToken() {
-// return token;
-// }
-
-// public void setToken(String token) {
-// this.token = token;
-// }
-// }
