4313ad28be4cd6ab5c90c83a52e7e5e11c737f2e
Hidenori Mishima <mishima.hidenori@gmail.com>
2025-04-17 06:16:42 +0900

    jsファイルをコミット対象から除外。typescript(ts, tsx)ファイルのみコミットする。

--- frontend/src/hooks/useLoginUserContext.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import LoginUserContext from "../context/LoginUserContext";
-const useLoginUserContext = () => {
-    return useContext(LoginUserContext);
-};
-export default useLoginUserContext;
--- frontend/src/models/SampleModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-class SampleModel {
-    constructor(name) {
-        this.name = name;
-    }
-}
