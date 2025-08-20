43b55794b0996b6dd66b513f150e27457ab8ccf8
Hidenori Mishima <mishima.hidenori@gmail.com>
2025-08-20 10:04:10 +0900

    警告を解消する。「将来のバージョン v7 での仕様変更に対する警告」

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,25 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import { Provider } from "./context/LoginUserContext";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import { Provider } from './context/LoginUserContext';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 if (rootElement) {
   createRoot(rootElement).render(
-    <BrowserRouter future={{ v7_startTransition: true }}>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true, // ← 警告回避用フラグ
+      }}
+    >
       <Provider>
         <App />
       </Provider>
     </BrowserRouter>
   );
 } else {
-  console.error("Failed to find the root element.");
+  console.error('Failed to find the root element.');
 }
